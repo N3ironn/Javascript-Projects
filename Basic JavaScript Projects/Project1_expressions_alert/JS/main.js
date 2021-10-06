a20077e22c9e4701aff49af6a84157971d3cecb0
N3ironn <81843074+N3ironn@users.noreply.github.com>
2021-10-06 15:58:15 -0700

    multi-variable

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,3 @@
-var X = "Tom" + "\"Iceman\"" + "Kazanski"
-document.write(X)
+var Genre = "EDM", Subgenre = "Bass House, Trap", Album1 = "Renaissance", 
+    Album2 = "New Worlds", Album3 = "Two Hearts";
+document.write(Album1);
