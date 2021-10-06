dd109f3e2ce4ebf0b576c877cbd68914d885589c
N3ironn <81843074+N3ironn@users.noreply.github.com>
2021-10-06 16:09:30 -0700

    font-color

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,8 @@
 var Genre = "EDM", Subgenre = "Bass House, Trap", Album1 = "Renaissance", 
     Album2 = "New Worlds", Album3 = "Two Hearts";
-document.write(Album1);
+var Genre = Genre.fontcolor("Green");
+var Subgenre = Subgenre.fontcolor("yellow");
+var Album1 = Album1.fontcolor("red");
+var Album2 = Album2.fontcolor("pink");
+var Album3 = Album3.fontcolor("purple");
+document.write(Album2);
