fb9818c55c24178c5360bec1d1e5805826cde0f1
N3ironn <81843074+N3ironn@users.noreply.github.com>
2021-10-06 13:58:40 -0700

    apostrophes

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,11 +1,11 @@
 
-var N = 'Strings'
-document.write(N)
 
 /*window alert with variables*/
 var X = 'Bill Burr';
 window.alert('Who is the best comedian?\n click ok to find out')
 window.alert(X)
+
+document.write('John Wick\'s one liners \"Everything\'s Got A Price.\"')
 
 
 
