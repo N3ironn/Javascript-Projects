237a68225f7145ca97798ededd7baba9ba53803a
N3ironn <81843074+N3ironn@users.noreply.github.com>
2021-10-06 16:37:04 -0700

    javascript expressions

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,8 +1,2 @@
-var Genre = "EDM", Subgenre = "Bass House, Trap", Album1 = "Renaissance", 
-    Album2 = "New Worlds", Album3 = "Two Hearts";
-var Genre = Genre.fontcolor("Green");
-var Subgenre = Subgenre.fontcolor("yellow");
-var Album1 = Album1.fontcolor("red");
-var Album2 = Album2.fontcolor("pink");
-var Album3 = Album3.fontcolor("purple");
-document.write(Album2);
+document.write(22 * 8);
+
