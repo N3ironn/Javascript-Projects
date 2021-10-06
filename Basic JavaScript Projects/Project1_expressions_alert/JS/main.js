80b89b7bc7568de14f5d7516a0d3cd1c4ceac01f
N3ironn <81843074+N3ironn@users.noreply.github.com>
2021-10-06 15:28:52 -0700

    concatenation

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,13 +1,1 @@
-
-
-/*window alert with variables*/
-var X = 'Bill Burr';
-window.alert('Who is the best comedian?\n click ok to find out')
-window.alert(X)
-
-document.write('John Wick\'s one liners \"Everything\'s Got A Price.\"')
-
-
-
-
-
+document.write("John Wick\'s one liners \"You wanted me Back\," + " I\'m Back!\"");
