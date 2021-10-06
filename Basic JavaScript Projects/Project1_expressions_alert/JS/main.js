278a0b6693c2b9d1611872daac823da21de491b2
N3ironn <81843074+N3ironn@users.noreply.github.com>
2021-10-06 13:41:55 -0700

    Window alert variable output

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,13 @@
-document.write('hello world')
+
+var N = 'Strings'
+document.write(N)
+
+/*window alert with variables*/
+var X = 'Bill Burr';
+window.alert('Who is the best comedian?\n click ok to find out')
+window.alert(X)
+
+
+
+
+
