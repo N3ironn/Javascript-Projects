d45a0c6a73257288a1f19e261f0b7620fda0f13a
N3ironn <81843074+N3ironn@users.noreply.github.com>
2021-10-06 17:03:24 -0700

    Basic Javascript Projects Submission Assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,17 @@
+document.write('hello world')
+var X = 'Bill Burr'; //naming a variable
+window.alert('Who is the best comedian?\n click ok to find out') // pop up window with alert and statement or response 
+window.alert(X) //getting a pop up window
+document.write('John Wick\'s one liners \"Everything\'s Got A Price.\"') //using /' to let the computer know that ' is important
+document.write("John Wick\'s one liners \"You wanted me Back\," + " I\'m Back!\""); //concatenating and using the escape character /"
+var X = "Tom" + "\"Iceman\"" + "Kazanski" //concatenating strings with variable
+document.write(X)//calling variable
+var Genre = "EDM", Subgenre = "Bass House, Trap", Album1 = "Renaissance", 
+    Album2 = "New Worlds", Album3 = "Two Hearts"; //multi-variable strings
+var Genre = Genre.fontcolor("Green"); //changing font color
+var Subgenre = Subgenre.fontcolor("yellow"); //changing font color
+var Album1 = Album1.fontcolor("red"); //changing font color
+var Album2 = Album2.fontcolor("pink"); //changing font color
+var Album3 = Album3.fontcolor("purple"); //changing font color
+document.write(Album2); //calling a variable
 document.write(22 * 8); //using document.write to multiply and expression
-
