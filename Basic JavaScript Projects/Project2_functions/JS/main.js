b39e69a118f1d4dea01f062d9146cd1449fa11c5
N3ironn <81843074+N3ironn@users.noreply.github.com>
2021-10-06 20:05:28 -0700

    challenge concatenate

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,11 @@
-function My_First_Function() {
-    var str = "This text is green!";
-    var result = str.fontcolor("green");
-    document.getElementById("Green_text").innerHTML = result;
+function My_First_Function() { //naming function
+    var str = "This text is orange!"; //setting variable string
+    var result = str.fontcolor("orange"); //setting font
+    document.getElementById("Orange_Text").innerHTML = result; //executing function
+}
+
+function myFunction() {
+    var sentence = "Yin";
+    sentence += "Yang";
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
