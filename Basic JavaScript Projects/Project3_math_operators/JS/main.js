ecde312a3abae88eed6b691766bc36076812b9f1
N3ironn <81843074+N3ironn@users.noreply.github.com>
2021-10-09 11:53:43 -0700

    listing variables as dictionaries

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -58,6 +58,18 @@ function calculateCircumference() { //naming the function
     document.getElementById("calculateCircumference").innerHTML = Pie //calling the function and making the element equal to variable
 }
 
+function my_Dictionary() { //named function
+    var animal = { //named the catagory of information 
+        species: "dog", //specific info
+        color: "black", //
+        breed: "labrador", // 
+        age: 5, //
+        sound: "bark!" //
+    };
+    document.getElementById("dictionary").innerHTML = animal.color; //calls the function finds the element and result is equal to the variable specified. 
+    //first place marks the catagory period marks the end and second work names the specific data
+}
+
 
 
 
