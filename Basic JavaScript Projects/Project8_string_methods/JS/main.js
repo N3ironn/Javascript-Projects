4a2b8a484943f3a77b4c579aecee392265a58938
N3ironn <81843074+N3ironn@users.noreply.github.com>
2021-10-13 16:34:56 -0700

    methods

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -33,8 +33,19 @@ let str = "Fall is Here"; //declares a string.
 document.getElementById("fall").innerHTML = str.search('is'); //finds ID, the str.search looks for a particular string and finds what is specified within the paraenthesis. and its character start #
 
 
-function string_Method() {
-    var X = 182;
-    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+function string_Method() {//function name
+    var X = 182;//variable and value. single = (double == if you want the function to compare to another value)
+    document.getElementById("Numbers_to_string").innerHTML = X.toString(); //searchs for ID element and equates .innerhtml to the X variable.to string (its supposed to return  an object as its string representation according to google)
 }
 
+function precision_Method() {//naming the function
+    var X = 12938.3012987376112;//naming variable X and giving it a value
+    document.getElementById("Precision").innerHTML = X.toPrecision(9);//toPrecision method formats it to specific length placed in the () in this example 10 characters not including the decimal 
+}//also looks like it rounds up that 9 to make the 2 into a 3. 
+
+let digit = 8.88674; //let is kind of like variable but used under different conditions, variables are used in functions, let used in block statements.
+let x1 = digit.toFixed(3); //toFixed rounds the decimal point to 3rd place as specified in the (). 
+document.getElementById("twix").innerHTML = x1;
+
+let PBC = "Peanut Butter Cups";
+document.getElementById("reeses").innerHTML = PBC.valueOf();
